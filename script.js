a28444259a8fbe35a2f84cb8877e2b2d127522b2
John Nkp <john.nkp1@gmail.com>
2020-12-25 13:00:06 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+const btn = document.querySelector('.btn')
+const greet = document.querySelector('h1')
+const reset = document.querySelector('.reset-btn')
+
+
+btn.addEventListener('click', () =>{
+    let load = 0
+    let int = setInterval(blurring, 30)
+
+    function new_fuc() {
+        load++
+        if (load == 90) {
+            // action
+            greet.classList.toggle('display')
+            btn.classList.add('display-none')
+            reset.classList.add('display')
+        }
+        
+    }
+
+    setInterval(new_fuc, 50)
+
+    function blurring() {
+        load++
+    
+        if (load > 99) {
+            clearInterval(int)
+        }
+    
+        loadText.innerText = `${load}%`
+        loadText.style.opacity = scale(load, 30, 100, 1, 0)
+        bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    
+    }
+})
+
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
+reset.addEventListener('click', () =>{
+    btn.classList.add('display')
+    reset.classList.remove('display')
+    reset.classList.add('display-none')
+    greet.classList.toggle('display')
+
+})
